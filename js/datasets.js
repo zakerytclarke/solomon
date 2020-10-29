6c4ec5bf29de884165b5dd6235e45b5fe3a1c91d
Zakery Clarke <zakerytclarke@gmail.com>
2020-10-29 10:22:13 -0600

    Added default datasets

--- js/datasets.js
+++ js/datasets.js
@@ -1,4 +1,22 @@
-var IRIS_DATA={
-  data:[],
-  preferecnes:{}
+function loadFromURL(url){
+  fetch(url)
+  .then(response => response.json())
+  .then(function(data){
+    loadSolomon(data)
+    autoSanitize();
+    changePage('home');
+  });
+}
+
+function loadSolomon(data){
+  var ld=JSON.parse(data);
+  //Load data into program
+  DATA=ld.DATA;
+  STRUCTURE=ld.STRUCTURE;
+  MODELS=ld.MODELS;
+  for(var key in MODELS.query){
+    //Extract function from string
+    eval("var tempFunc="+MODELS.query[key]);
+    MODELS.query[key]=tempFunc;
+  }
 }
